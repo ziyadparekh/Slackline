330f02152bc9bc317c5e7af86dfdf59511653313
Ziyad Parekh <ziyad@1stdibs.com>
2014-10-07 12:53:31 -0400

    new font Lato, working facebook login

--- express-app.js
+++ express-app.js
@@ -11,10 +11,11 @@ var toobusy = require('toobusy');
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
 var app = express();
 
 //Routes & API
-var categories = require("./routes/api/categories");
+var users = require('./routes/api/users');
 var index = require('./routes/index');
 //dont crash on overload
 app.use(function(req, res, next) {
@@ -30,27 +31,30 @@ process.on('uncaughtException', function(err){
     process.exit(0);
 });
 
+//static and port
+app.enable('trust proxy', 1);
+app.set('port', process.env.PORT || 3010);
+app.set('view engine', 'ejs');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 if(process.env.PORT){
     app.use(morgan('combined'));
 }else{
     app.use(morgan('combined'));
-    app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(errorHandler({ dumpExceptions: false, showStack: false }));
     app.use(session({
-        secret: 'tobo!',
-        maxAge: 36000009,
-        saveUninitialized: true,
-        resave: true,
-        cookie: {
-            maxAge: 60 * 60 * 10008,
-            secure: true
-        }
+        secret: 'tobo2obo',
+        cookie: { maxAge: 60 * 60 * 10008 }
     }));
 }
 
 //error management
 app.use(function(err, req, res, next){
     if(err.stack)
-        console.log(err.stack);
+        //console.log(err.stack);
     if(err && err.redirect)
         return res.redirect(err.redirect);
     else if(err == "503")
@@ -61,10 +65,6 @@ app.use(function(err, req, res, next){
         return res.send("An error occured. Please try again in a few seconds.");
 });
 
-//static and port
-app.enable('trust proxy');
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
 if(process.env.PORT){
     app.set('views', __dirname + '/dist/views');
     app.use(express.static(path.join(__dirname, 'dist/public')));
@@ -72,16 +72,22 @@ if(process.env.PORT){
     app.set('views', __dirname + '/views');
     app.use(express.static(path.join(__dirname, 'public')));
 }
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Passport configuration
+require('./modules/auth');
+
 //routes
-//categories
-app.post("/"+version+"/categories", categories.create);
-app.get("/"+version+"/categories", categories.list);
-app.get('*', index.index);
+//logins
+app.get('/login', index.login);
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successReturnToOrRedirect: '/', failureRedirect: '/login' }));
+//logout
+app.get('/logout', index.logout);
+//status
+app.get('/status', ensureLoggedIn('/login'), index.status);
+app.get('/*', ensureLoggedIn('/login'), index.index);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,33 @@
+//Module dependencies
+var pjson = require('../package.json');
+
 exports.index = function(req, res){
     if(req)
         var user = JSON.stringify(req.user);
     var url = "http://localhost:3010";
     res.render('slackline',{url:url, user: user});
+};
+
+exports.login = function(req, res){
+    var url = "http://localhost:3010";
+    res.render('login', {url: url});
 }
+exports.logout = function(req, res){
+    if(req && req.user && req.user.id){
+        req.session.destroy();
+        res.redirect('/login');
+    }else
+    res.redirect('/login');
+};
+
+exports.status = function(req, res){
+    res.send({meta: 200,
+        response: {
+            name: process.env.DB_NAME+"-web",
+            status: "ok",
+            message: "All systems go!",
+            version: pjson.version,
+        },
+        error: null
+    });
+};
